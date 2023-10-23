9fafaf95dd780739694dddae32e150cedddbbea9
CCELEND <celend2644884626@163.com>
2023-10-23 21:02:59 +0800

    cgdb.cc commit

--- cgdb.cc
+++ cgdb.cc
@@ -3,7 +3,7 @@
 #include "./elf/loader_elf.h"
 #include "./disasm/disasm.h"
 
-// 定义两个全局变量
+// 定义全局变量
 vector<string> myargv;
 string cmd;
 map<string, unsigned long long> fun_plt;
@@ -24,11 +24,13 @@ int main(int argc, char *argv[])
         err_exit("Program loading failed!");
     }
 
-    while (true){
+    map_fun_plt(fname);
+
+    while (true) {
         printf("\033[34m\033[1mcgdb> \033[0m");
         getline(cin, cmd);
 
-        if (cmd == "q"){
+        if (cmd == "q") {
             goto cgdb_exit;
         } else if (cmd == "symbol" || cmd == "sym") {
             show_elf_symbol(&bin);
@@ -40,6 +42,8 @@ int main(int argc, char *argv[])
             show_elf_got(fname);
         } else if (cmd == "plt") {
             show_elf_plt(fname);
+        } else if (cmd == "lplt") {
+            show_elf_lib_plt();
         } else if (cmd == "r") {
             run_dyn_debug(fname, &bin);
         } else {
