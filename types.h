b459a9e85cec8eeeb4b2408185abacb9f2bd6db7
CCELEND <celend2644884626@163.com>
2023-11-14 18:39:09 +0800

    types.h commit

---
+++ types.h
@@ -0,0 +1,17 @@
+#ifndef _HAVE_TYPES_H
+#define _HAVE_TYPES_H
+
+#include <stdint.h>
+#include <stdlib.h>
+
+// typedef int8_t   s8;
+// typedef int16_t  s16;
+typedef int       s32;
+typedef long long s64;
+
+// typedef uint8_t  u8;
+// typedef uint16_t u16;
+typedef unsigned int u32;
+typedef unsigned long long u64;
+
+#endif
