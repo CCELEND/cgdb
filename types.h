e50e6dd1b5a09488a790e683cfd4c94e39ac33c4
CCELEND <celend2644884626@163.com>
2023-11-14 18:41:54 +0800

    types.h commit

--- types.h
+++ types.h
@@ -9,9 +9,9 @@ typedef int16_t   s16;
 typedef int       s32;
 typedef long long s64;
 
-typedef uint8_t				u8;
-typedef uint16_t 			u16;
-typedef unsigned int 		u32;
-typedef unsigned long long 	u64;
+typedef uint8_t             u8;
+typedef uint16_t            u16;
+typedef unsigned int        u32;
+typedef unsigned long long  u64;
 
 #endif
