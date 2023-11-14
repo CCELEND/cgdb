8a95db2f3df78a7456a5d12d041248fe186b05aa
CCELEND <celend2644884626@163.com>
2023-11-14 18:39:09 +0800

    need_include.h commit

---
+++ need_include.h
@@ -0,0 +1,31 @@
+
+#ifndef NEED_INCLUDE
+#define NEED_INCLUDE
+
+#include <stdio.h>
+#include <errno.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <termios.h>
+
+#include <sys/ptrace.h>
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <sys/wait.h>
+#include <sys/user.h>
+#include <sys/reg.h>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <map>
+
+#include "./types.h"
+
+#endif
