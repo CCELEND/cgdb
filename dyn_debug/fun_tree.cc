fffc8ae72f7b39741d1bc0dbd049b83020331d09
CCELEND <celend2644884626@163.com>
2023-11-16 13:36:07 +0800

    fun commit

--- dyn_debug/fun_tree.cc
+++ dyn_debug/fun_tree.cc
@@ -9,8 +9,8 @@ fun_tree_node_t* sub_node;
 void set_sub_node_link(string sub_fun_name)
 {
     fun_tree_node_t* temp = NULL;
-    sub_node = (fun_tree_node_t*)malloc(sizeof(struct fun_tree_node));
-    memset(sub_node, 0, sizeof(struct fun_tree_node));
+    sub_node = (fun_tree_node_t*)calloc(1, sizeof(struct fun_tree_node));
+    // memset(sub_node, 0, sizeof(struct fun_tree_node));
 
     sub_node->fun_info.fun_name = sub_fun_name;
     sub_node->next = NULL;
@@ -45,11 +45,10 @@ void set_sub_node_link(string sub_fun_name)
 void tree_disasm(pid_t pid, char* byte_codes, u64 parent_fun_addr, s32 num, string parent_fun_name)
 {
     csh handle;
-    cs_insn *insn;
+    cs_insn* insn;
     size_t count;
     u64 sub_fun_addr;
     string sub_fun_name;
-    // s32 offset;
 
     if (cs_open(CS_ARCH_X86, CS_MODE_64, &handle) != CS_ERR_OK) {
         printf("\033[31m\033[1m[-] Failed to initialize Capstone!\033[0m\n");
@@ -65,6 +64,7 @@ void tree_disasm(pid_t pid, char* byte_codes, u64 parent_fun_addr, s32 num, stri
             if (!strcmp(insn[j].mnemonic, "bnd jmp") || !strcmp(insn[j].mnemonic, "call"))
             {
                 printf("%s, %s\n",insn[j].mnemonic, insn[j].op_str);
+
                 u64 fun_start_addr;
                 if (!strcmp(insn[j].mnemonic, "bnd jmp"))
                 {
@@ -114,8 +114,8 @@ s32 set_parent_node(pid_t pid, char* parent_fun_name)
     // 8字节对齐
     fun_size = fun_size + LONG_SIZE - fun_size % LONG_SIZE;
 
-    parent_node = (fun_tree_node_t*)malloc(sizeof(struct fun_tree_node));
-    memset(parent_node, 0, sizeof(struct fun_tree_node));
+    parent_node = (fun_tree_node_t*)calloc(1, sizeof(struct fun_tree_node));
+    // memset(parent_node, 0, sizeof(struct fun_tree_node));
 
     parent_node->next = NULL;
     parent_node->sub_fun = NULL;
