97793b9854978ce14bc8fcee86cddecf7929d888
CCELEND <celend2644884626@163.com>
2023-11-21 21:15:13 +0800

    fun commit

--- dyn_debug/fun_tree.cc
+++ dyn_debug/fun_tree.cc
@@ -19,7 +19,6 @@ fun_tree_node_t* creat_node(u64 addr)
 
     // try{}
     node = new fun_tree_node_t;
-    // printf("666\n");
     if (!node) {
         printf("[-] Failed to create node!\n");
         return NULL;
@@ -46,10 +45,7 @@ void insert_sub_link(u64 sub_fun_addr, fun_tree_node_t* parent_node)
 
     if (!parent_node->sub_fun)
     {
-        // printf("create\n");
         sub_node = creat_node(sub_fun_addr);
-        // printf("head: %s---\n", sub_node->fun_info.fun_name.c_str());
-        // printf("%p\n", sub_node);
         parent_node->sub_fun = sub_node;
         parent_node->sub_fun_num++;
         return;
@@ -66,10 +62,7 @@ void insert_sub_link(u64 sub_fun_addr, fun_tree_node_t* parent_node)
 
         if (!temp->next)
         {
-            // printf("create\n");
             sub_node = creat_node(sub_fun_addr);
-            // printf("%s-------\n", sub_node->fun_info.fun_name.c_str());
-            // printf("%p\n", sub_node);
             temp->next = sub_node;
             parent_node->sub_fun_num++;
             break;
@@ -85,7 +78,6 @@ void parent_disasm(pid_t pid, char* byte_codes, u64 parent_fun_addr, s32 parent_
 
     // printf("%d\n", parent_fun_size);
     count = cs_disasm(handle, (uint8_t*)byte_codes, parent_fun_size, parent_fun_addr, 0, &insn);
-    // printf("999\n");
 
     if (count > 0) {
         size_t j;
@@ -117,8 +109,6 @@ void parent_disasm(pid_t pid, char* byte_codes, u64 parent_fun_addr, s32 parent_
         cs_free(insn, count);
     }
     else printf("\033[31m\033[1m[-] Failed to disassemble given code!\033[0m\n");
-
-    // cs_close(&handle);
 }
 
 // 建立根节点
@@ -180,15 +170,11 @@ void free_fun_tree()
     vector<fun_tree_node_t*> sib_link_next_node;
     fun_tree_node_t *temp = NULL, *parent_node = root_node;
 
-    // printf("free-----------------\n");
-
     while(parent_node)
     {
-        // printf("%s\n", parent_node->fun_info.fun_name.c_str());
         // 当前节点兄节点压栈
         sib_link_next_node.push_back(parent_node->next);
         temp = parent_node;
-        // printf("%p\n", temp);
 
         // 当前节点有子
         if (parent_node->sub_fun)
@@ -198,17 +184,14 @@ void free_fun_tree()
         // 当前节点无子
         else
         {
-            // printf("free no sub: %ld\n", sib_link_next_node.size());
             // 弹出空值
             while (!sib_link_next_node.back())
             {
                 sib_link_next_node.pop_back();
             }
-            // printf("freeno: %ld\n", sib_link_next_node.size());
 
             if (!sib_link_next_node.size())
             {
-                // printf("free3\n");
                 memset(temp, 0, sizeof(struct fun_tree_node));
                 delete temp;
                 break;
@@ -218,7 +201,6 @@ void free_fun_tree()
             sib_link_next_node.pop_back();
         }
 
-        // printf("%p\n", temp);
         memset(temp, 0, sizeof(struct fun_tree_node));
         delete temp;
     }
@@ -247,9 +229,6 @@ void show_fun_tree(s32 level)
         // 当前节点兄弟节点压栈
         sib_link_next_node.push_back(temp_node->next);
         // 输出当前节点
-        // for (int i = 0; i < depth-1; i++) printf("│   ");
-
-        // for (int i = 0; i < depth-1; i++) 
         for (int i = 1; i < depth; i++)
         {
             if (sib_link_next_node[i])
@@ -258,8 +237,6 @@ void show_fun_tree(s32 level)
                 printf("     ");
         }
 
-        // for (int i = 0; i < depth-1; i++) printf("    ");
-        // temp_node->fun_info.fun_start_addr
         if (temp_node->next)
             printf(" ├─── %s\n", temp_node->fun_info.fun_name.c_str());
             // printf("├── %s 0x%llx-0x%llx\n", temp_node->fun_info.fun_name.c_str(), 
@@ -301,8 +278,6 @@ void creat_fun_tree(pid_t pid, s32 level)
     vector<fun_tree_node_t*> sib_link_next_node;
     fun_tree_node_t* parent_node = root_node;
     int current_depth = 0;
-
-    // printf("%ld\n", sizeof(struct fun_tree_node));
 
     while(parent_node)
     {
